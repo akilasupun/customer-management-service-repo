d37c28a0e6f59d0dcdc5bcae8c834f075f355520
Akila <akilas@gmail.com>
2019-11-06 20:10:52 +0530

    Renamed the main class & updated pom.xml

--- customer-management-service/src/main/java/com/rental/core/CustomerManagementServiceApplication.java
+++ customer-management-service/src/main/java/com/rental/core/CustomerManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.rental.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerManagementServiceApplication {
 
 	public static void main(String[] args) {
